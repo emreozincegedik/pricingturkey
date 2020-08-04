046c99f61d7bc94a5ac31cd879abb1737d7ce478
Emre Özincegedik <emreozincegedik@gmail.com>
2020-08-04 13:02:12 +0300

    kartlar dinamikliğe hazır

--- react/src/components/Bulten/BultenCard.js
+++ react/src/components/Bulten/BultenCard.js
@@ -3,10 +3,10 @@ import React, { Component } from "react";
 export class BultenCard extends Component {
   render() {
     return (
-      <div class={"order-" + this.props.class} style={{ width: "350px" }}>
-        <div class="card mb-4 bg-white" style={{ border: "hidden" }}>
+      <div className={"order-" + this.props.class} style={{ width: "350px" }}>
+        <div className="card mb-4 bg-white" style={{ border: "hidden" }}>
           <svg
-            class="bd-placeholder-img card-img-top"
+            className="bd-placeholder-img card-img-top"
             width="100%"
             height="225"
             xmlns="http://www.w3.org/2000/svg"
@@ -15,24 +15,27 @@ export class BultenCard extends Component {
             role="img"
             aria-label="Placeholder: Thumbnail"
           >
-            <title>Placeholder</title>
+            <title>{this.props.title || "Placeholder"}</title>
             <rect width="100%" height="100%" fill="#55595c" />
             <text x="50%" y="50%" fill="#eceeef" dy=".3em">
-              Thumbnail
+              {this.props.thumbnail || "Thumbnail"}
             </text>
           </svg>
-          <div class="card-body">
-            <p class="card-text">
-              This is a wider card with supporting text below as a natural
+          <div className="card-body">
+            <p className="card-text">
+              {this.props.text ||
+                `This is a wider card with supporting text below as a natural
               lead-in to additional content. This content is a little bit
-              longer.
+              longer.`}
             </p>
-            <div class="d-flex justify-content-between align-items-center">
-              <button type="button" class="btn btn-m btn-outline-vahitcan">
-                View
+            <div className="d-flex justify-content-between align-items-center">
+              <button type="button" className="btn btn-m btn-outline-vahitcan">
+                {this.props.btnText || "View"}
               </button>
 
-              <small class="text-muted">9 mins</small>
+              <small className="text-muted">
+                {this.props.date || "9 mins"}
+              </small>
             </div>
           </div>
         </div>

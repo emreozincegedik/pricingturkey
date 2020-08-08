e0abd0e42d041ee97fe5ab361e4688f8b6a2fbc3
Emre Özincegedik <emreozincegedik@gmail.com>
2020-08-08 15:09:37 -0700

    veri tabanı bağlantısı çalışıyor + docker

--- db/index.js
+++ db/index.js
@@ -8,12 +8,31 @@ app.use(cors());
 app.use(express.json());
 
 var config = {
-  server: process.env.server,
-  user: process.env.user,
-  password: process.env.password,
-  database: process.env.database,
+  server: "localhost",
+  user: "sa",
+  password: "Wdv3GrgqYDJzMLhYi0ek",
+  database: "pricingTurkey",
+  options: {
+    enableArithAbort: true,
+  },
   connectionTimeout: 150000,
 };
+
+// const run = async () => {
+//   let pool;
+//   try {
+//     console.log("Bağlantı açılıyor");
+//     pool = await sql.connect(config);
+//     const { res } = await sql.query`select * from users;`;
+//     console.log(res);
+//   } catch (err) {
+//     console.log(err);
+//   } finally {
+//     await pool.close();
+//     console.log("Bağlantı kapandı");
+//   }
+// };
+// run();
 // app.listen(port, () => {
 //   console.log(`Server is running on port: ${port}`);
 // });
@@ -29,7 +48,7 @@ sql
     return pool
       .request()
       .input("input_parameter", sql.Int, 1)
-      .query("select * from adminLogin where id = @input_parameter");
+      .query("select * from test where id = @input_parameter ");
   })
   .then((result) => {
     console.dir(result);

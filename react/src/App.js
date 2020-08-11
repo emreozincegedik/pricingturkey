8037a1d403b7608ec990fffdb59a8c91a29fc850
Emre Özincegedik <emreozincegedik@gmail.com>
2020-08-10 22:14:18 -0700

    header controlu elimizde

--- react/src/App.js
+++ react/src/App.js
@@ -3,13 +3,39 @@ import "./App.css"; //eklemek istediklerin
 //import "bootstrap/dist/css/bootstrap.min.css";
 import "./main.css"; //yeni bootstrap
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Anasayfa, Test, Test2, Login, Error,Hizmetlerimiz } from "./sayfalar";
+import { Anasayfa, Test, Test2, Login, Error, Hizmetlerimiz } from "./sayfalar";
 import { Navbars, Genel, Footer } from "./components";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <Router>
       <Genel>
+        <Helmet>
+          <meta charset="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <meta
+            name="description"
+            content="Vahitcan Tutar ve Emre Özincegedik tarafından yapılmıştır"
+          />
+          <meta
+            name="keywords"
+            content="HTML, CSS, JavaScript, React, Express"
+          />
+          <meta name="author" content="Emre Özincegedik Vahitcan Tutar" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Satisfy|Montserrat|Raleway&display=swap"
+            rel="stylesheet"
+          />
+          <title>Transfet Pricing Turkey</title>
+          <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          />
+        </Helmet>
         <Navbars />
         <Switch>
           <Route path="/" exact component={Anasayfa} />

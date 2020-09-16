d968c0246e2623f7f1751a978fc0db2781109e6c
Emre Özincegedik <emreozincegedik@gmail.com>
2020-09-16 12:49:25 -0700

    svg

--- react/src/sayfalar/Error.js
+++ react/src/sayfalar/Error.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Context } from "../components";
+import err from "../resimler/404.svg";
 export class Error extends Component {
   static contextType = Context;
   componentDidMount() {
@@ -8,8 +9,13 @@ export class Error extends Component {
   render() {
     const { dil_degisken } = this.context.state;
     return (
-      <div className="container" /* style={{ marginTop: "20vh" }} */>
-        <h1>404 {dil_degisken("sayfa bulunamadı", "page not found")}</h1>
+      <div className="container-fluid" /* style={{ marginTop: "20vh" }} */>
+        <img src={err} alt="" srcset="" />
+        <div class="container">
+          <h1 className="display-2">
+            {dil_degisken("sayfa bulunamadı", "page not found")}
+          </h1>
+        </div>
       </div>
     );
   }
